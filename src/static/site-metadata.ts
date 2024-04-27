506124b8230e64d306b03e07446354546babac81
queyiqin <queyiqin01@gmail.com>
2024-04-27 18:09:00 +0800

    2

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -10,15 +10,12 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
-  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
-  description: 'Personal site and blog',
+  siteTitle: 'Qiue的跑步记录',
+  siteUrl: '#',
+  logo: '#',
+  description: 'Qiue的跑步记录',
   navLinks: [
-    {
-      name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
-    },
+   
     {
       name: 'About',
       url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
